ae576d62e3a83a642cb6e569d7245706cb26d110
Avinash Kaitha <avinashkaitha93@gmail.com>
2015-08-25 16:43:13 +0530

    DetailActivityFragment width updated

--- app/src/main/java/in/mediabin/mediabin/DetailActivityFragment.java
+++ app/src/main/java/in/mediabin/mediabin/DetailActivityFragment.java
@@ -3,6 +3,7 @@ package in.mediabin.mediabin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,9 +34,16 @@ public class DetailActivityFragment extends Fragment {
         TextView summaryTextView = (TextView) rootView.findViewById(R.id.summary);
         summaryTextView.setText(summary);
         ImageView imageView = (ImageView) rootView.findViewById(R.id.backgrnd);
+        DisplayMetrics metrics = new DisplayMetrics();
+        double aspectRatio = 0.5;
+        getActivity().getWindowManager()
+                .getDefaultDisplay()
+                .getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = (int) (width * aspectRatio);
         Picasso.with(getActivity())
                 .load(backgrnd_url)
-                .resize(480,240)
+                .resize(width,height)
                 .centerCrop()
                 .into(imageView);
 
